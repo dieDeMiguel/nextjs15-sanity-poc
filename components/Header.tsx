cd75e3f375982eb3a9e4e73f2ee0b0b0f6cf9192
Progress with the component <deMiguel@newsaktuell.de>
2024-11-30 16:57:22 +0100

    Add Prettier configuration and VSCode settings; format code and update imports

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,21 @@
-"use client";
-import Link from "next/link";
-import { useUser } from "@clerk/nextjs";
-import Form from "next/form";
+'use client';
+import Link from 'next/link';
+import { useUser } from '@clerk/nextjs';
+import Form from 'next/form';
+import { TrolleyIcon } from '@sanity/icons';
 
 export default function Header() {
   const { user } = useUser();
 
   return (
-    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+    <header className="flex flex-wrap items-center justify-between px-4 py-2">
       <div>
-        <Link
-          href="/"
-          className="
-            text-2xl
-            font-bold
-            text-blue-500
-            hover:opacity-50
-            cursor-pointer
-            mx-auto
-            sm:mx-0
-            "
-        >
+        <Link href="/" className="mx-auto cursor-pointer text-2xl font-bold text-blue-500 hover:opacity-50 sm:mx-0">
           Shopr
         </Link>
         <Form
           action="/search"
-          className="
-            w-full
-            sm:w-auto
-            sm:flex-1
-            sm:mx-4
-            mt-2
-            sm:mt-0
-            "
+          className="mt-2 w-full sm:mx-4 sm:mt-0 sm:w-auto sm:flex-1"
           name="query"
           placeholder="Search for products"
         >
@@ -40,22 +23,18 @@ export default function Header() {
             type="text"
             name="query"
             placeholder="Search for products"
-            className="
-            bg-gray-100
-            text-gray-800
-            px-4
-            py-2
-            rounded
-            focus:outline-none
-            focus:ring-2
-            focus:ring-blue-500
-            focus:ring-opacity-50
-            border
-            w-full
-            max-w-4xl
-            "
+            className="w-full max-w-4xl rounded border bg-gray-100 px-4 py-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           />
         </Form>
+        <div>
+          <Link
+            href="/basket"
+            className="relative flex flex-1 items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 sm:flex-none sm:justify-start"
+          >
+            <TrolleyIcon className="h-6 w-6" />
+            <span>My Basket</span>
+          </Link>
+        </div>
       </div>
     </header>
   );
