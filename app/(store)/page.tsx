e5884cf61c394a8253d436d143f871815ee2a2c8
Progress with the component <deMiguel@newsaktuell.de>
2024-12-01 07:07:05 +0100

    Add product fetching functionality and update schema for sales and orders

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { getAllProducts } from '@/sanity/lib/getAllProducts';
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+  console.log('products:', products);
   return (
     <div>
       <h1 className="main">Hello World!</h1>
