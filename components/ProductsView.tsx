86c3a2ec6f65d33128547f5d0c469237efb97428
Progress with the component <deMiguel@newsaktuell.de>
2024-12-02 19:50:22 +0100

    Add category selector and dialog components; update ProductsView to include category filtering

--- components/ProductsView.tsx
+++ components/ProductsView.tsx
@@ -1,15 +1,18 @@
 import { Category, Product } from '@/sanity.types';
 import ProductGrid from './ProductGrid';
+import { CategorySelectorComponent } from './category-selector';
 
 interface ProductsViewProps {
   products: Product[];
   categories: Category[];
 }
 
-const ProductsView = ({ products }: ProductsViewProps) => {
+const ProductsView = ({ products, categories }: ProductsViewProps) => {
   return (
     <div className="flex flex-col">
-      {/* <div className="w-full sm:w-[200px]"><CategorySelectorComponent categories={categories} /></div> */}
+      <div className="w-full sm:w-[200px]">
+        <CategorySelectorComponent categories={categories} />
+      </div>
       <div className="flex-1">
         <div>
           <ProductGrid products={products} />
