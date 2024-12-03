42fbe1ffbbfa2d2b3eee0191fbd8680508dc2c4d
Progress with the component <deMiguel@newsaktuell.de>
2024-12-03 11:26:41 +0100

    added loading state

---
+++ app/(store)/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-blue-500"></div>
+    </div>
+  );
+}
