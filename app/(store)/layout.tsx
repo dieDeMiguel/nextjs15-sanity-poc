d2ad2cd1069585de77392bdf6bbfd1c7a5aa9d28
Progress with the component <deMiguel@newsaktuell.de>
2024-12-04 10:52:05 +0100

    Add Vercel Analytics integration to layout and update dependencies

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -3,6 +3,7 @@ import '../globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import Header from '@/components/Header';
 import { SanityLive } from '@/sanity/lib/live';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,6 +24,7 @@ export default function RootLayout({
             {children}
           </main>
           <SanityLive />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
